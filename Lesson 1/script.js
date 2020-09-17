3f1fb567c8f2baa3f72cbbb1900c649d4f5354f0
Ekaterina Erokhina <katerina.shimshek@gmail.com>
2020-09-17 23:30:54 +0300

    lesson 1 complete

---
+++ Lesson 1/script.js
@@ -0,0 +1,19 @@
+// задание 1
+var tempC = 25;
+var tempF = (9 / 5) * tempC + 32;
+alert(tempF);
+
+// задание 2
+var admin;
+var name = 'Василий';
+admin = name;
+alert(admin);
+
+// задание 3
+alert(1000 + '108'); //1000108
+
+// задание 4 (обмен переменными)
+var a = 10;
+var b = 20;
+a = b;
+b = a / 2;
