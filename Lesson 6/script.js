be2ec1a57bf46b724cdcf612719b5a48cc58aa53
Ekaterina Erokhina <katerina.shimshek@gmail.com>
2020-10-07 16:04:21 +0300

    lesson 6 homework fixed

--- Lesson 6/script.js
+++ Lesson 6/script.js
@@ -1,17 +1,18 @@
 function init() {
     var images = document.getElementsByClassName("sm-img");
-    for (var i = 0; i <= images.length; i++) {
-        images.onclick[i] = changeBigPicture;
+    for (var i = 0; i < images.length; i++) {
+        images[i].onclick = changeBigPicture;
     }
 }
 
 function changeBigPicture(eventObj) {
-    var bigPicsDiv = document.getElementsByClassName("big-pics");
+    var bigPicsDiv = document.getElementsByClassName("big-pics")[0];
     bigPicsDiv.innerHTML = "";
     var eventElement = eventObj.target;
     var imagePartName = eventElement.id.split("_");
     var src = "images/big-images/" + imagePartName[1] + ".jpg";
     var newBigPic = document.createElement("img");
+    newBigPic.className = "bg-img";
     newBigPic.src = src;
     bigPicsDiv.appendChild(newBigPic);
     if (eventElement = null) {
